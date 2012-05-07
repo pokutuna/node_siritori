448535c3eab2405b7e8844f692691aa950880033
pokutuna <popopopopokutuna@gmail.com>
2012-05-07 13:54:55 +0900

    change port 3000 to 9393

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.configure('production', function(){
 
 app.get('/', routes.index);
 
-app.listen(3000, function(){
+app.listen(9393, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 });
 
